760d8a6072d10ef9e43c2dc2e5bd9cc6497cee3b
Даниил Захаров <daniilzakharov2005@gmail.com>
2026-04-10 22:15:27 +0300

    lab2 fix

--- TrayService/SessionLauncher.cpp
+++ TrayService/SessionLauncher.cpp
@@ -40,7 +40,7 @@ void LaunchAppInSession(DWORD sessionId)
     wcscat_s(szPath, L"TrayApp.exe");
 
     wchar_t szCmdLine[MAX_PATH + 32]{};
-    swprintf(szCmdLine, MAX_PATH + 32, L"\"%s\" --hidden", szPath);
+    _snwprintf(szCmdLine, MAX_PATH + 32, L"\"%s\" --hidden", szPath);
 
     STARTUPINFOW si{};
     si.cb          = sizeof(si);
